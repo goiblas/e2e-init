3c29f8fa55e22a41834d5d1ebeda76f655f2a13c
Joan León <joan.leon@gmail.com>
2022-11-19 10:23:45 +0100

    Add new script to record videos & publish to dasboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,11 +1,11 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: '',
-  reporter: 'cypress-mochawesome-reporter',
+  projectId: "et3bg9",
+  reporter: "cypress-mochawesome-reporter",
   e2e: {
     setupNodeEvents(on, config) {
-      require('cypress-mochawesome-reporter/plugin')(on);
+      require("cypress-mochawesome-reporter/plugin")(on);
     },
   },
 });
