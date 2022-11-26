c56dee6b45233bd9e83328ad73896d75e4294ad6
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2022-11-26 10:27:14 +0100

    fix cypress pattern

--- cypress.config.js
+++ cypress.config.js
@@ -7,7 +7,7 @@ module.exports = defineConfig({
     setupNodeEvents(on, config) {
       require("cypress-mochawesome-reporter/plugin")(on);
     },
-    specPattern: "cypress/e2e/**/*/.{cy,spec}.{js,jsx,ts,tsx}",
+    specPattern: "cypress/e2e/**/*.{cy,spec}.{js,jsx,ts,tsx}",
     baseUrl: "https://example.cypress.io",
     pageLoadTimeout: 1000,
     viewportHeight: 950,
