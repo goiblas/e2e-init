ab6be2ccfe79d25ed2279ae6ec806b23cf0a4d26
Jesús Olazagoitia <jesus.olazagoitia@gmail.com>
2022-11-26 11:00:12 +0100

    remove proyect id

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,6 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  projectId: "et3bg9",
   reporter: "cypress-mochawesome-reporter",
   e2e: {
     setupNodeEvents(on, config) {
